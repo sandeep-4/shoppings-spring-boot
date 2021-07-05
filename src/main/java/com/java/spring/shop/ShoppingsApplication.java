50cd4aead721c8d49482b06d285ca8caa674845a
SANDEEP <shwandeep@gmail.com>
2021-07-05 15:30:49 +0545

    first commit

---
+++ src/main/java/com/java/spring/shop/ShoppingsApplication.java
@@ -0,0 +1,13 @@
+package com.java.spring.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingsApplication.class, args);
+	}
+
+}
